a8d5291d023e75b67dad9e34cc8fef474cb82d66
ajaxj <ajaxj@icloud.com>
2026-09-02 13:49:37 +0800

    add

--- spbackend/src/lib/api.ts
+++ spbackend/src/lib/api.ts
@@ -1,7 +1,7 @@
 // API 基础地址
 // export const API_BASE_URL = "https://data.zhibo20.com";
-export const API_BASE_URL = "https://gin1.zhibo20.com";
-// export const API_BASE_URL = "http://gin2.marstommy.serv00.net"
+// export const API_BASE_URL = "https://gin1.zhibo20.com";
+export const API_BASE_URL = "http://gin2.marstommy.serv00.net"
 
 // API 接口端点常量
 export const API_ENDPOINTS = {
